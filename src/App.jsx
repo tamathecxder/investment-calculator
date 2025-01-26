5e0184d82e586976216be5b0484ef748f4735811
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-26 21:21:46 +0700

    implement state and handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import Header from './components/Header'
-import Table from './components/Table'
-import Forms from './components/Forms'
+import Header from "./components/Header";
+import Table from "./components/Table";
+import Forms from "./components/Forms";
+import { useState } from "react";
+import { calculateInvestmentResults } from "./util/investment";
 
 function App() {
+  const [investment, setInvestment] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+    result: [],
+  });
+
+  function handleInputForms(key, value) {
+    setInvestment((previousInvestment) => {
+      const investmentObject = { ...previousInvestment };
+
+      if (investmentObject.hasOwnProperty(key)) {
+        const result = calculateInvestmentResults(investmentObject);
+
+        investmentObject[key] = Number(value) || 0;
+        investmentObject["result"] = result;
+      }
+
+      return investmentObject;
+    });
+  }
+
   return (
-    <main className='center'>
+    <main className="center">
       <Header />
-      <Forms />
-      <Table />
+      <Forms onInputForms={handleInputForms} />
+      <Table result={investment.result} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
