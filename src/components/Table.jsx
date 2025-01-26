72402be9eab7ac9c4aa2d9536375fd6371291522
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-26 11:01:59 +0700

    init table structure

---
+++ src/components/Table.jsx
@@ -0,0 +1,24 @@
+export default function Table() {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2025</td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
