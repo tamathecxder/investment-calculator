7dd77805be12c3a89db8c552af445439e2b5f793
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-26 21:21:21 +0700

    attach an event on each input

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,25 +1,47 @@
-export default function Forms() {
+export default function Forms({ onInputForms }) {
+  function handleCalculations(event, key) {
+    const value = event.target.value;
+
+    onInputForms(key, value);
+  }
+
   return (
     <section id="user-input" className="user-input-container">
       <div className="input-group">
         <div>
           <label>Initial Investment</label>
-          <input type="text" name="initial_investment" />
+          <input
+            type="number"
+            name="initial_investment"
+            onChange={(event) => handleCalculations(event, "initialInvestment")}
+          />
         </div>
         <div>
           <label>Annual Investment</label>
-          <input type="text" name="annual_investment" />
+          <input
+            type="number"
+            name="annual_investment"
+            onChange={(event) => handleCalculations(event, "annualInvestment")}
+          />
         </div>
       </div>
 
       <div className="input-group">
         <div>
           <label>Expected Return</label>
-          <input type="text" name="expected_return" />
+          <input
+            type="number"
+            name="expected_return"
+            onChange={(event) => handleCalculations(event, "expectedReturn")}
+          />
         </div>
         <div>
-          <label>Duration</label>
-          <input type="text" name="duration" />
+          <label>Duration</label>{" "}
+          <input
+            type="number"
+            name="duration"
+            onChange={(event) => handleCalculations(event, "duration")}
+          />
         </div>
       </div>
     </section>
