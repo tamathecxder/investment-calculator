639a6a5613581e7d34a887ec08a5002a43cbad68
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-01-27 11:05:14 +0700

    update logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
 export default function Header() {
   return (
     <header id="header">
-      <img src="../../public/investment-calculator-logo.png" alt="Logo" />
-      <h1>React Investment Calculator</h1>
+      <img src={logo} alt="Logo" />
+      <h1>Investment Calculator</h1>
     </header>
   );
 }
